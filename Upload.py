2c190494e9621eac03c008f85084cb0e687127a7
Martin <Martin@woelfer.org>
2015-08-25 10:13:35 +0200

    modify Upload.py

--- Upload.py
+++ Upload.py
@@ -11,61 +11,7 @@ class FileUpload(object):
     @cherrypy.expose
     def index(self):
         #Open the index webpage
-        return """
-            <html>
-                <body>
-                <h2>Upload a file</h2>
-                <link rel="stylesheet" href="stylesheet.css">
-                <script type="text/javascript">
-
-                    // Check for the various File API support.
-                    if (window.File && window.FileReader && window.FileList && window.Blob) {
-                    // Great success! All the File APIs are supported.
-                    } else {
-                        alert('The File APIs are not fully supported in this browser.');
-                    }
-                </script>
-
-                    <form action="upload" method="post" enctype="multipart/form-data" name="uploadForm" id="uploadForm">
-                        <div id="drop_zone">Drop files here</div>
-                        <!-- Damit ich einen Parameter übergeben kann -->
-                        <input type="hidden" name="myFile" id="myFile" value="">
-                        <script>
-                            function handleFileSelect(evt) {
-                                //Methoden damit der Normalablauf gestoppt wird
-                                evt.stopPropagation();
-                                evt.preventDefault();
-
-                                //Ein File Array erstellen
-                                var files = evt.dataTransfer.files;
-                                //File aus dem Array nehmen
-                                var file = files.item(0);
-                                //zum testen
-                                alert(file.name);
-                                alert(file.size);
-                                //value von dem versteckten input feld auf das File setzen (Dieser Schritt funktioniert anscheinend nicht)
-                                document.getElementById("myFile").setAttribute('value', file);
-
-                                alert(document.getElementById("myFile").value.name); //Gibt undefined aus -> funktioniert nicht :(
-                                //Automatisches submitten bei Drag and Drop
-                                document.getElementById('uploadForm').submit();
-                            }
-
-                            function handleDragOver(evt) {
-                                evt.stopPropagation();
-                                evt.preventDefault();
-                                evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-                            }
-
-                            // Setup the dnd listeners.
-                            var dropZone = document.getElementById('drop_zone');
-                            dropZone.addEventListener('dragover', handleDragOver, false);
-                            dropZone.addEventListener('drop', handleFileSelect, false);
-                        </script>
-                    </form>
-                </body>
-            </html>
-            """
+        return open("index.html")
 
     def upload(self, myFile):
         out = """<html>
